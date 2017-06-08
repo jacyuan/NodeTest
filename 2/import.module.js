b763880b1fda1e285b920b0d2ff3ece0ce700cc8
Yuan LIN <ayuan19841021@hotmail.com>
2017-06-08 23:02:46 +0200

    lesson 2 : modules

---
+++ 2/import.module.js
@@ -0,0 +1,6 @@
+//we import two modules, with each one representing just a single text
+const module1 = require('./module1');
+const module2 = require('./module2');
+
+//we output them in console log
+console.log(`${module1} ${module2}`);
---
+++ 2/module2.js
@@ -0,0 +1,2 @@
+//in this file, we just export a single text
+module.exports = 'world';
