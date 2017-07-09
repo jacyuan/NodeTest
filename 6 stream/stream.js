a975e92bd04f9aec53c8be423eee7f950b058a6e
Yuan LIN <ayuan19841021@hotmail.com>
2017-07-09 22:59:29 +0200

    lesson 6 : stream

---
+++ 6 stream/stream.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+readTest();
+
+// writeTest();
+
+function readTest() {
+    //a read stream is an event emitter that emits readable and end events.
+    const stream = fs.createReadStream(
+        './1.txt',
+        {encoding: 'utf8'}
+    );
+
+    stream.on('readable', function () {
+        let chunk;
+
+        while (chunk = stream.read()) {
+            console.log(chunk);
+        }
+    });
+
+    stream.once('end', function () {
+        console.log('Read file over');
+    });
+}
+
+function writeTest() {
+    const stream = fs.createWriteStream('./out.txt');
+
+    for (let i = 0; i < 10; i++) {
+        stream.write(`line ${i}\n`);
+    }
+}
